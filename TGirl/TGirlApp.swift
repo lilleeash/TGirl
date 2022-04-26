09c4fb5ecae0c7eecae27ac3f80a4abd111f4546
Darya <lilleeash@icloud.com>
2022-04-26 18:07:06 +0300

    Initial Commit

---
+++ TGirl/TGirlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TGirlApp.swift
+//  TGirl
+//
+//  Created by Darya Zhitova on 26.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TGirlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
